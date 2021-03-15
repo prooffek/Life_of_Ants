8cc617752756924c9c14523d3b66fed03a8a5c4a
appuser <appuser@journey2-5fc55b6689-gs9rl>
2021-03-15 17:27:08 +0000

    starter project

---
+++ src/Codecool.LifeOfAnts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Codecool.LifeOfAnts
+{
+    /// <summary>
+    ///     Simulation main class
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        ///     Main method
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello, Ants!");
+        }
+    }
+}
