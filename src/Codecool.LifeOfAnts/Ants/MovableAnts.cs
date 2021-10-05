b62b81a2c8533870e7c51e1ff15ef2cc4aa00d2b
KT <prooffek@gmail.com>
2021-10-05 14:30:09 +0200

    Abstract MovableAnt class added for Drone, Worker and Soldier

---
+++ src/Codecool.LifeOfAnts/Ants/MovableAnts.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Codecool.LifeOfAnts.Ants
+{
+    public abstract class MovableAnts : Ant
+    {
+        public abstract void Move();
+        
+        protected void SetStartingPosition()
+        {
+            var x = _random.Next(Colony.Width);
+            var y = _random.Next(Colony.Width);
+            Position = new Position(x, y);
+        }
+
+        public void ChangePosition(int positionX, int positionY)
+        {
+            Colony.FindSquare(positionX, positionY).RemoveAnt(this);
+            var newX = ManagePositionOnBoard(positionX);
+            var newY = ManagePositionOnBoard(positionY);
+            Position = new Position(newX, newY);
+            Colony.FindSquare(Position.X, Position.Y);
+        }
+
+        protected int ManagePositionOnBoard(int position)
+        {
+            if (position < 0)
+                return 0;
+
+            if (position >= Colony.Width)
+                return Colony.Width - 1;
+
+            return position;
+        }
+
+        public override void ActOnUpdate()
+        {
+            Move();
+        }
+    }
+}
